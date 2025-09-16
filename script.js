2eda51ebd62196428e5088cb129187cfa05d06d3
samuelbonifacio015 <samuelbonifacio015@gmail.com>
2025-09-16 17:22:23 -0500

    feat: added responsive navbar & updated UI

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-const hamburger-menu = document.getElementById('hamburger-menu');
-const nav-links = document.getElementById('nav-links');
 
-hamburger-menu.addEventListener('click', () => {
-    nav-links.classList.toggle('active');
+/**
+ * Creacion de boton hamburguesa
+ */
+
+const btn = document.querySelector(".menu-toggle");
+const menu = document.querySelector(".nav-links");
+
+//EventListener para click 
+btn.addEventListener("click", ()=> {
+    //Al dar click en el icono de menu hamburguesa, se alterna
+    menu.classList.toggle("active");
 });
+
+//Se cierra el menu-horizontal al hacer otro click
+document.querySelectorAll(".nav-links li a").forEach((link) =>
+  link.addEventListener("click", () => {
+    menu.classList.remove("active");
+  })
+);
